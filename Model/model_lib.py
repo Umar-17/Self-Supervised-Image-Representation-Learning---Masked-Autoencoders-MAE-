b8ad1f6d22530430d818daca81c30bf1614deb2b
Umar Zahoor <umarzahoor017@gmail.com>
2026-03-10 04:20:05 +0500

    Fix: Indented forward method inside class
    forward method missing in model_lib.py file

--- Model/model_lib.py
+++ Model/model_lib.py
@@ -15,7 +15,6 @@ class MaskedAutoencoder(nn.Module):
     def __init__(self):
         super().__init__()
         
-   
         self.patch_embed = PatchEmbed(img_size=224, patch_size=16, embed_dim=768)
         self.cls_token = nn.Parameter(torch.zeros(1, 1, 768))
         self.pos_embed = nn.Parameter(torch.zeros(1, self.patch_embed.num_patches + 1, 768))
@@ -23,14 +22,12 @@ class MaskedAutoencoder(nn.Module):
         encoder_layer = nn.TransformerEncoderLayer(d_model=768, nhead=12, dim_feedforward=3072, activation='gelu', batch_first=True)
         self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=12) 
         
-        
         self.decoder_embed = nn.Linear(768, 384) 
         self.mask_token = nn.Parameter(torch.zeros(1, 1, 384)) 
         self.decoder_pos_embed = nn.Parameter(torch.zeros(1, self.patch_embed.num_patches + 1, 384))
         
         decoder_layer = nn.TransformerEncoderLayer(d_model=384, nhead=6, dim_feedforward=1536, activation='gelu', batch_first=True)
         self.decoder = nn.TransformerEncoder(decoder_layer, num_layers=12)
-        
         
         self.decoder_pred = nn.Linear(384, 16**2 * 3) 
 
@@ -54,7 +51,6 @@ class MaskedAutoencoder(nn.Module):
     def forward_encoder(self, x, mask_ratio):
         x = self.patch_embed(x)
         x = x + self.pos_embed[:, 1:, :]
-        
         x, mask, ids_restore = self.random_masking(x, mask_ratio)
         
         cls_token = self.cls_token + self.pos_embed[:, :1, :]
@@ -66,7 +62,6 @@ class MaskedAutoencoder(nn.Module):
 
     def forward_decoder(self, x, ids_restore):
         x = self.decoder_embed(x)
-        
         mask_tokens = self.mask_token.repeat(x.shape[0], ids_restore.shape[1] + 1 - x.shape[1], 1)
         x_ = torch.cat([x[:, 1:, :], mask_tokens], dim=1)
         x_ = torch.gather(x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))
@@ -74,6 +69,13 @@ class MaskedAutoencoder(nn.Module):
         x = torch.cat([x[:, :1, :], x_], dim=1)
         x = x + self.decoder_pos_embed
         x = self.decoder(x)
-        
         x = self.decoder_pred(x)
-        return x
+        
+     
+        return x[:, 1:, :]
+
+    def forward(self, imgs, mask_ratio=0.75):
+        
+        latent, mask, ids_restore = self.forward_encoder(imgs, mask_ratio)
+        pred = self.forward_decoder(latent, ids_restore)
+        return pred, mask
